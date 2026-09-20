a0268765fd13a9fa3318436d5991c2c98ca1564f
medvedack <medvedack@gmail.com>
2026-09-20 22:59:31 +0500

    Add lab00, lab01

---
+++ sem-1-lab-00/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CsLabs;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        string buffer = Console.ReadLine();
+        int age = Convert.ToInt32(buffer);
+        Console.WriteLine("==Player's game passport==");
+        Console.WriteLine($"Name: {name}");
+        Console.Write($"Age: {age}");
+    }
+}
---
+++ sem-1-lab-01-server-monitoring/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CsLabs;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the server name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter the count of active servers: ");
+        byte activeServers = byte.Parse(Console.ReadLine());
+        byte maxServers = 8;
+
+        Console.Write("Enter the current online players count: ");
+        int currentOnlineCount = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter RAM in use (GB): ");
+        float usedRam = float.Parse(Console.ReadLine());
+        float totalRam = 16.0f;
+
+        Console.Write("Enter CPU temperature (°C): ");
+        double cpuTemperature = double.Parse(Console.ReadLine());
+
+        Console.Write("Is the server online? (true/false): ");
+        bool isOnline = bool.Parse(Console.ReadLine());
+
+        Console.WriteLine("==Server Information:==");
+        Console.WriteLine($"Server Name: {name}");
+        Console.WriteLine($"Active Servers: {activeServers}/{maxServers}");
+        Console.WriteLine($"Current Online Count: {currentOnlineCount}");
+        Console.WriteLine($"RAM Usage: {usedRam}/{totalRam} GB");
+        Console.WriteLine($"CPU Temperature: {cpuTemperature} °C");
+        Console.WriteLine($"Is Online: {isOnline}");
+
+    }
+}
